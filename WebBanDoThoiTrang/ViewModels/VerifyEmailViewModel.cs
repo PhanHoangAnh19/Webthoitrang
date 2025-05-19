efccd66a52eb48644d6737717771b59aafdc7be7
Phan Hoang Anh <angphan5@gmail.com>
2025-05-19 22:13:09 +0700

    Initial commit

---
+++ WebBanDoThoiTrang/ViewModels/VerifyEmailViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebBanDoThoiTrang.ViewModels
+{   // xac thuc email
+    public class VerifyEmailViewModel
+    {
+
+        [Required(ErrorMessage = "Bắt buộc nhập Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
